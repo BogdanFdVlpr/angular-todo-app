fc550b5df0ccb0bcb27a602969ff4e2b9c70ba17
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2023-07-26 15:31:30 +0300

    add style and work with forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+let initialTodos = [
+  { id: 1, title: 'HTML + CSS', completed: true},
+  { id: 2, title: 'JS', completed: false},
+  { id: 3, title: 'React', completed: false},
+  { id: 4, title: 'Vue.js', completed: false}
+]
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+  
 export class AppComponent {
-  title = 'angular-todo-app';
+  editing = false;
+  todos = initialTodos;
+  title = '';
+
+  get activeTodos() {
+    return this.todos.filter(todo => !todo.completed)
+  }
+
+  addTodo() {
+    if (!this.title) {
+      return;
+    }
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: this.title,
+      completed: false,
+    };
+
+    this.todos.push(newTodo);
+    this.title = '';
+  }
 }
